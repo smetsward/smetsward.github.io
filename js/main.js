02e98f68e04048350bc324ef77ebc413bd81e558
smetsward <wardsmets.code@gmail.com>
2022-11-19 23:35:12 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+/*function alpineInstance() {
+  return {
+    tickets: [],
+  }
+}*/
+
 function alpineInstance() {
   return {
     tickets: [],
+    init() {
+      this.getTickets()
+    },
+    getTickets() {
+        fetch ('https://userticket-api-service-smetsward.cloud.okteto.net/tickets/?skip=0&limit=100')
+          .then(response => response.json())
+          .then(data => this.tickets = data.tickets)
+    }
   }
 }
